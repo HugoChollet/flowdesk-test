9ea95b976453d45be3e9456e0bb66cb80f8e6e9f
HugoChollet <tibolescabo@gmail.com>
2023-05-28 18:06:35 +0200

    update pages to fetch data instead of mocked one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectChangeEvent } from "@mui/material";
 
 import styles from "./page.module.css";
-import { Selector } from "./Selector.component";
-import { COIN_LIST } from "./CoinListData";
+import { Selector } from "./component/Selector.component";
+import { getData } from "./api/getData";
 
 type CurrencyType = {
   from: string;
@@ -12,23 +12,34 @@ type CurrencyType = {
 };
 
 export default function Home() {
-  const [currency, setCurrency] = useState<CurrencyType>({ from: "", to: "" });
+  const [coinList, setCoinList] = useState<Array<string>>();
+  const [currency, setCurrency] = useState<CurrencyType>();
+
+  useEffect(() => {
+    getData("https://api.binance.com/api/v1/exchangeInfo").then((data) => {
+      setCoinList(data.symbols.map((item: any) => item.symbol));
+    });
+  }, []);
 
   const handleChange = (event: SelectChangeEvent<string>, label: string) => {
     const {
       target: { value },
     } = event;
-    console.log(label, value);
     if (label === "From :") {
-      setCurrency({ from: value, to: currency.to });
-    } else if (label === "To :")
-      setCurrency({ from: currency.from, to: value });
+      setCurrency({ from: value, to: currency?.to || "" });
+    } else if (label === "To :") {
+      setCurrency({ from: currency?.from || "", to: value });
+    }
   };
 
   return (
     <div className={styles.main}>
-      <Selector label="From :" values={COIN_LIST} onChange={handleChange} />
-      <Selector label="To :" values={COIN_LIST} onChange={handleChange} />
+      <Selector
+        label="From :"
+        values={coinList || []}
+        onChange={handleChange}
+      />
+      <Selector label="To :" values={coinList || []} onChange={handleChange} />
     </div>
   );
 }
