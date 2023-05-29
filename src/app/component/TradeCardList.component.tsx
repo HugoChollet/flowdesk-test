a1655ce69877c0477302a87551a81f1d6d70074a
HugoChollet <tibolescabo@gmail.com>
2023-05-29 13:58:14 +0200

    implement filter to sort list

--- src/app/component/TradeCardList.component.tsx
+++ src/app/component/TradeCardList.component.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import FilterButtons from "./FilterButtons.component";
 import TradeCard from "./TradeCard.component";
 import { TradeData } from "./TradeData.type";
+
+export type FilterType = null | "time" | "price" | "qty";
 
 type Props = {
   tradeDataArray: TradeData[];
@@ -8,9 +11,24 @@ type Props = {
 };
 
 const TradeCardList: React.FC<Props> = ({ tradeDataArray, name }) => {
+  const [filter, setFilter] = useState<FilterType>(null);
+
+  const compare = (a: any, b: any) => {
+    if (filter) {
+      if (a[filter] < b[filter]) return -1;
+      if (a[filter] > b[filter]) return 1;
+    }
+    return 0;
+  };
+
+  tradeDataArray.sort(compare);
+
   return (
     <div>
-      <div>{name} :</div>
+      <div>
+        <span>Data for {name}, </span>
+        filter by <FilterButtons filter={filter} setFilter={setFilter} />
+      </div>
       {tradeDataArray.map((tradeData) => (
         <TradeCard key={tradeData.id} tradeData={tradeData} />
       ))}
